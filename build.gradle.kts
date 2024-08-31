6383ea42445cffbc6cfe0c3459530574b83f1a00
david-o-rosario1201 <finnnn023@gmail.com>
2024-08-31 11:52:26 -0400

    Agregando plugins y versiones

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     id("androidx.room") version "2.6.1" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
 }
