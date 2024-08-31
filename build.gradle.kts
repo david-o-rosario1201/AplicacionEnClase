60548a7ad211ca62de3d649b13a2f7fe7b7da8c1
david-o-rosario1201 <finnnn023@gmail.com>
2024-08-31 10:56:08 -0400

    Agregando plugins al gradle del proyecto

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
     id("androidx.room") version "2.6.1" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
